71c72d9f6a73ce569a2730ba7f7d606c6f6d0673
unknown <sk8801@srmist.edu.in>
2026-02-26 16:09:24 +0530

    Srilekha Kramadhati added UC-4

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,28 +1,42 @@
-import java.util.Scanner;
 public class PalindromeCheckerApp {
 
+    /**
+     * Application entry point for UC4.
+     * @param args Command-line arguments
+     */
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a string to check if it is a palindrome: ");
-        String original = scanner.nextLine();
+        String input = "radar";
 
-        String reversed = "";
 
-        // Iterate the string in reverse order
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed += original.charAt(i);
+        char[] chars = input.toCharArray();
+
+
+        int start = 0;
+
+        int end = chars.length - 1;
+
+
+        boolean isPalindrome = true;
+
+
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
         }
 
-        // Compare original and reversed strings
-        if (original.equalsIgnoreCase(reversed)) {
-            System.out.println("Result: \"" + original + "\" is a palindrome.");
+
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
         } else {
-            System.out.println("Result: \"" + original + "\" is NOT a palindrome.");
+            System.out.println(input + " is not a palindrome.");
         }
-
-        scanner.close();
-
     }
 }
